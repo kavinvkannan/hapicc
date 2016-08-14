f6e042b0f4a92f63c8b6a9ee734e15b8a0edd766
Michael Gregory <mgregory8219@gmail.com>
2016-08-14 03:27:41 -0400

    added new token for fitbit

--- app/models/fitbit.rb
+++ app/models/fitbit.rb
@@ -1,8 +1,7 @@
 class Fitbit < ApplicationRecord
 	def send_api_request
-		headers = {"Authorization" => "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0U01WS04iLCJhdWQiOiIyMjhWN0QiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd3dlaSB3c29jIHdhY3Qgd3NldCB3bG9jIiwiZXhwIjoxNDcxMTU4NTMwLCJpYXQiOjE0NzExMjk3MzB9.rfSQFQZmKSehmScSYUxcERxCrXXWubNKMUFdLGl-dKk"}
+		headers = {"Authorization" => "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyNzkyNVAiLCJhdWQiOiIyMjhWN0QiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd3dlaSB3c29jIHdhY3Qgd3NldCB3bG9jIiwiZXhwIjoxNDcxMTg4MjMxLCJpYXQiOjE0NzExNTk0MzF9.m1aJ_CciOgOwYu82xyoUSEbcQtXzYA7ekAMvNdXB2HQ"}
 		@response = HTTParty.get('https://api.fitbit.com/1/user/-/profile.json', headers: headers).response.body
-		
 	end
 
 	def parse
