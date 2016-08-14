6d8ad363172eb039d762efe0e5d039beed67099f
Michael Gregory <mgregory8219@gmail.com>
2016-08-13 23:33:31 -0400

    fixed httparty request in fitbit model

--- app/models/fitbit.rb
+++ app/models/fitbit.rb
@@ -1,10 +1,14 @@
 class Fitbit < ApplicationRecord
 	def send_api_request
 		headers = {"Authorization" => "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0U01WS04iLCJhdWQiOiIyMjhWN0QiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd3dlaSB3c29jIHdhY3Qgd3NldCB3bG9jIiwiZXhwIjoxNDcxMTU4NTMwLCJpYXQiOjE0NzExMjk3MzB9.rfSQFQZmKSehmScSYUxcERxCrXXWubNKMUFdLGl-dKk"}
-		@response = HTTParty.get('https://api.fitbit.com/1/user/-/profile.json', headers: headers)
+		@response = HTTParty.get('https://api.fitbit.com/1/user/-/profile.json', headers: headers).response.body
 	end
 
 	def parse
-		@parsed_response = JSON.parse @response
+		@parsed_response = (JSON.parse @response)['user']
+	end
+
+	def age
+		@parsed_response['age']
 	end
 end
